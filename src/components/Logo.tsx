d894bc0998c3e86b34844754a80e8c02787d33a3
Hernán <hj@expeditech.net>
2023-05-24 17:14:48 -0300

    Added dummy logos

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,13 +4,13 @@ import logoMinimalLight from "./../assets/logo-minimal-light.svg";
 import logoMinimalDark from "./../assets/logo-minimal-dark.svg";
 export function LogoText(props: JSX.IntrinsicElements["svg"]) {
   return (
-    <img src={logoDark} style={{width: "100%", height: "100%", padding: "1rem"}}></img>
+    <img src={logoDark} style={{width: "100%", height: "100%", padding: "0.5em"}}></img>
   );
 }
 
 export function LogoTextLight(props: JSX.IntrinsicElements["svg"]) {
   return (
-    <img src={logoLight} style={{width: "100%", height: "100%", padding: "1rem"}}></img>
+    <img src={logoLight} style={{width: "100%", height: "100%", padding: "0.5em"}}></img>
   );
 }
 
@@ -28,12 +28,12 @@ export function LogoLight(props: JSX.IntrinsicAttributes["svg"]) {
 
 export function LogoIcon(props: JSX.IntrinsicElements["svg"]) {
   return (
-    <img src={logoMinimalDark} style={{width: "38px", height: "38px", padding: "7px"}}></img>
+    <img src={logoMinimalDark} style={{width: "38px", height: "38px"}}></img>
   );
 }
 
 export function LogoIconLight(props: JSX.IntrinsicElements["svg"]) {
   return (
-    <img src={logoMinimalLight} style={{width: "38px", height: "38px", padding: "7px"}}></img>
+    <img src={logoMinimalLight} style={{width: "38px", height: "38px"}}></img>
   );
 }
